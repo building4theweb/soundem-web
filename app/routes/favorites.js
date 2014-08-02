98f8a56a0ee43ffff6fb1c96945eba63aa6daaf6
Giovanni Collazo <hello@gcollazo.com>
2014-08-02 18:12:03 -0400

    Fix songs view with new song component

--- app/routes/favorites.js
+++ app/routes/favorites.js
@@ -5,5 +5,12 @@ export default Ember.Route.extend({
     // TODO: Fix query
     // return this.store.find('song', {favorite: true});
     return this.store.find('song');
+  },
+
+  setupController: function(controller, model) {
+    this._super(controller, model);
+
+    // reset selected song
+    controller.set('songSelected', null);
   }
 });
