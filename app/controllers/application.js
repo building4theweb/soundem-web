c6ee4ac22c3e5417701e7361ab1048d26997b9aa
Giovanni Collazo <hello@gcollazo.com>
2014-08-02 21:48:09 -0400

    Adds basic display

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   needs: ['audio'],
-  audioIsPlaying: Ember.computed.alias('controllers.audio.isPlaying')
+  audioIsPlaying: Ember.computed.alias('controllers.audio.isPlaying'),
+  songInPlayer: Ember.computed.alias('controllers.audio.songInPlayer')
 });
