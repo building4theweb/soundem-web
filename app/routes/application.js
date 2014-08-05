992986acfbcf7e2bbdf0b2f6dcd3097eff1f07ff
Giovanni Collazo <hello@gcollazo.com>
2014-08-05 13:24:20 -0400

    If user gets to /music redirect to /music/albums

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  setupController: function(controller, model) {
+    var self = this;
+
+    this._super(controller, model);
+
+    Ember.run.later(function() {
+      if (controller.currentRouteName === 'music.index') {
+        self.transitionTo('albums');
+      }
+    });
+  }
+});
