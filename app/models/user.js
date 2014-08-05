87bf875ed1298a4045bc75f377feb0023f5c1c7f
José Padilla <jpadilla@webapplicate.com>
2014-08-05 09:05:46 -0400

    Fix user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,17 +1,17 @@
 import DS from 'ember-data';
 
-var Email = DS.Model.extend({
-  email: DS.attr('email'),
+var User = DS.Model.extend({
+  email: DS.attr('string'),
   songTotal: DS.attr('number'),
   albumTotal: DS.attr('number'),
   durationTotal: DS.attr('number')
 });
 
-Email.reopenClass({
+User.reopenClass({
   FIXTURES: [
     {id: 1, songTotal: 11, albumTotal: 3, durationTotal: 1322},
     {id: 2, songTotal: 21, albumTotal: 23, durationTotal: 21322},
   ]
 });
 
-export default Email;
+export default User;
